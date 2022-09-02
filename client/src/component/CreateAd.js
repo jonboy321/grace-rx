c1fb50a507e35cd451cab43d8327528294496c75
Jonny Gemme <gemmejonathan@gmail.com>
2022-09-02 10:54:01 -0400

    styled create advocate

--- client/src/component/CreateAd.js
+++ client/src/component/CreateAd.js
@@ -43,89 +43,91 @@ function CreateAd() {
 	};
 
 	return (
-		<div>
+		<div className="formContainer">
 			<form className="form">
-				<input
+				<input id="mobile"
 					value={data.mobile}
 					name="mobile"
 					onChange={handleInputChange}
 					type="text"
 					placeholder="mobile"
 				/>
-				<input
+				<input id="fullname"
 					value={data.fullname}
 					name="fullname"
 					onChange={handleInputChange}
 					type="text"
 					placeholder="full name"
 				/>
-				<input
+				<input id="position"
 					value={data.position}
 					name="position"
 					onChange={handleInputChange}
 					type="text"
 					placeholder="position"
 				/>
-				<input
+				<input id="ss"
 					value={data.ss}
 					name="ss"
 					onChange={handleInputChange}
 					type="text"
 					placeholder="social security"
 				/>
-				<input
+				<input id="dob"
 					value={data.dob}
 					name="dob"
 					onChange={handleInputChange}
 					type="text"
 					placeholder="dob"
 				/>
-				<input
+				<input id="email"
 					value={data.email}
 					name="email"
 					onChange={handleInputChange}
 					type="text"
 					placeholder="email"
 				/>
-				<input
+				<input id="datestarted"
 					value={data.datestarted}
 					name="datestarted"
 					onChange={handleInputChange}
 					type="text"
 					placeholder="date started"
 				/>
-				<input
+				<input id="datevh"
 					value={data.datevh}
 					name="datevh"
 					onChange={handleInputChange}
 					type="text"
 					placeholder="date verified hippa"
 				/>
-				<input
+				<input id="saddress"
 					value={data.saddress}
 					name="saddress"
 					onChange={handleInputChange}
 					type="text"
 					placeholder="shipping address"
 				/>
-				<input
+				<input id="paddress"
 					value={data.paddress}
 					name="paddress"
 					onChange={handleInputChange}
 					type="text"
 					placeholder="personal address"
 				/>
-				<input
+				<input id="obmi"
 					value={data.obmi}
 					name="obmi"
 					onChange={handleInputChange}
 					type="text"
 					placeholder="onboard manager id"
 				/>
-				<button type="button" onClick={createadvo}>
+			</form>
+			<div className="btnContainer">
+				<button className="submitBtn" type="button" onClick={createadvo}>
 					Submit
 				</button>
-			</form>
+			</div>
 		</div>
 	);
 }
