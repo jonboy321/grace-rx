564d74256b1b8de79b833868839de0af1daf69a6
jahnathan x <jahnathanexantus@gmail.com>
2022-09-04 18:26:46 -0400

    created get request with axios and made it render on the page

--- client/src/component/AdProfile.js
+++ client/src/component/AdProfile.js
@@ -1,34 +1,38 @@
-import React, { useState } from "react";
+import React,{useEffect,useState} from "react";
 import axios from "axios";
 // import uuid from "react-uuid";
 
 function AdProfile() {
-	const [data, setData] = useState([]);
+	const [data, setData] = useState([])
+
+	useEffect(()=> {
+		axios.get("http://localhost:3001/api/advocate").then((res)=>{
+			console.log("getting from::::",res.data);
+			setData(res.data)
+		}).catch((err)=> {console.log(err)})
+			;
+		
+	},[])
+
+
 	
-
-	function displaypro() {
-		axios.get("http://localhost:3001/api/advocate", data).then((response) => {
-			console.log(response);
-			setData(response.data);
-		});
-	}
-
 	return (
-		<div className="app">
-		  <h2>All datas 📫</h2>
-		  {data.map((data) => {
-			 return (
-				<div className="data-card" key={data.id}>
-				   <h2 className="data-title">{data.fullname}</h2>
-				   <p className="data-body">{data.ss}</p>
-				   {/* <div className="button">
-					  <div className="delete-btn">Delete</div>
-				   </div> */}
-				</div>
-			 );
-		  })}
+		<div>
+			{data.map((data)=>{
+				return(
+					<table>
+  <tr>
+    <th>{data.fullname}</th>
+    <th>{data.mobile}</th>
+    <th>{data.position}</th>
+  </tr>
+ 
+</table>
+
+				)
+			})}
 		</div>
-	  );
+	);
 }
 
 export default AdProfile;
--- client/src/component/Buttons.js
+++ client/src/component/Buttons.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Nav from "react-bootstrap/Nav";
+
 
 
 
